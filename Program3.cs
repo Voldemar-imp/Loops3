8b17eba619abc076da1351d6b7ea06c9713d2738
Voldemar-imp <108013210+Voldemar-imp@users.noreply.github.com>
2022-07-04 20:01:19 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditionalOperatorsAndLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = 5;
+            int maxNumber = 96;
+            int step = 7;
+            int number;
+
+            for (number = minNumber; number <= maxNumber; number += step)
+            {
+                Console.WriteLine(number);                
+            }            
+        }
+    }
+}
